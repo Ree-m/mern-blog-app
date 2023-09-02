1aef57a05f53be91fd9f8cd0cadb592c5a78bdd3
Ree-m <reembsrat@gmail.com>
2023-09-02 07:46:01 +0300

    test

--- client/src/pages/Feature1.jsx
+++ client/src/pages/Feature1.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 
 const Feature1 = () => {
   return (
-<div>Fetaure3 merge conflict test</div>  )
+<div>Fetaure3 merge conflict test
+  <div>
+    test test
+  </div>
+</div>
+
+)
 }
 
 export default Feature1
